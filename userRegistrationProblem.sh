a90468ad9692d14294511d7804917a35fe398860
SanketTambe39 <sankettambe39@gmail.com>
2020-08-07 23:23:23 +0530

    Validation for Emails added

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,14 +1,24 @@
 #!/bin/bash -x
 
-read -p "Enter your First name :" firstName
-read -p "Enter your Last name :" lastName
+#read -p "Enter your First name :: " firstName
+#read -p "Enter your Last name :: " lastName
 
-namepattern="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
+read -p "Please Enter vaild email address ::" mail
 
-if [[ $firstName =~ $namepattern && $lastName =~ $namepattern  ]]
+#nameValidation="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
+
+#if [[ $firstName =~ $nameValidation && $lastName =~ $nameValidation  ]]
+#then
+#	echo " Your name is perfect "
+#else
+#	echo " Check your Name both should be start with upper case. "
+#fi
+
+mailValidation="^([a-zA-Z_]{1}[a-zA-Z0-9]+)(([_.-+]?[a-zA-Z0-9]+)*)@([a-z0-9]+)\.([a-zA-Z]{2,5})([.]{0,1}([a-zA-Z]{2,5})*)$"
+
+if [[ $mail =~ $mailValidation ]]
 then
-	echo " Your name is perfect "
+	echo " You just entered a valid email id "
 else
-	echo " Check your Name both should be start with upper case. "
+	echo " You just entered a invalid email id "
 fi
-
