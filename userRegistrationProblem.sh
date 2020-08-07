dba05a1e5a0d6a2837d7a19057983f4a0653859c
SanketTambe39 <sankettambe39@gmail.com>
2020-08-07 23:31:19 +0530

    Updated Email validation

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -x
 
-#read -p "Enter your First name :: " firstName
-#read -p "Enter your Last name :: " lastName
+read -p "Enter your First name :: " firstName
+read -p "Enter your Last name :: " lastName
 
 read -p "Please Enter vaild email address ::" mail
 
-#nameValidation="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
+nameValidation="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
 
-#if [[ $firstName =~ $nameValidation && $lastName =~ $nameValidation  ]]
-#then
-#	echo " Your name is perfect "
-#else
-#	echo " Check your Name both should be start with upper case. "
-#fi
+if [[ $firstName =~ $nameValidation && $lastName =~ $nameValidation  ]]
+then
+	echo " Your name is perfect "
+else
+	echo " Check your Name both should be start with upper case. "
+fi
 
 mailValidation="^([a-zA-Z_]{1}[a-zA-Z0-9]+)(([_.-+]?[a-zA-Z0-9]+)*)@([a-z0-9]+)\.([a-zA-Z]{2,5})([.]{0,1}([a-zA-Z]{2,5})*)$"
 
